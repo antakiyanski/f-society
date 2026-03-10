971cd87a91cc3ab4752364cf592a618a847cd47e
nilufarturdibaeva <nilufarturdibaeva9@gmail.com>
2026-03-10 09:51:35 +0500

    bnm

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 print("Anvar Tursunbayev")
 print('developer Rxt')
 print("aybek")
-<<<<<<< HEAD
 print("Malika")
-=======
 print("Dilnoza")
 print('xudaybergan')
->>>>>>> 0718bdae6892a681395877e8a9a006b6a540426a
+print("nilufar")
+
